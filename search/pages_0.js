1040d49c304b54ee5f30af843819afa5fa3ce911
tanquan <tanquanisslash@gmail.com>
2025-06-13 06:52:44 +0000

    Deploying to gh-pages from @ tanquan/synergy@8170dd4e9992c07ff5002d880cd076ccf78441bd 🚀

---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['building_20deskflow_0',['Building Deskflow',['../md_doc_2dev_2build.html',1,'']]]
+];
---
+++ search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deskflow_0',['Deskflow',['../md_doc_2dev_2build.html',1,'Building Deskflow'],['../md_doc_2dev_2contributing.html',1,'Contributing to Deskflow'],['../index.html',1,'Deskflow']]]
+];
